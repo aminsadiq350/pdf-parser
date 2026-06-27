37a638a62d238cf6bd7cb690669081e4f1cd9189
aminsadiq350 <aminsadiq@MacBook-Pro-16>
2026-06-27 19:08:52 +0400

    chore: configure eslint + prettier
    - ESLint flat config with @typescript-eslint, vue plugin, browser/node/vitest globals
    - Prettier with no-semi single-quote conventions
    - VS Code extension recs (Volar, Prettier, ESLint)
    - Adds vue-eslint-parser, @eslint/js, globals to devDependencies

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import vue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import globals from 'globals'
+
+export default [
+  { ignores: ['dist/**', 'node_modules/**', 'coverage/**'] },
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,vue}'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: { '@typescript-eslint': tseslint, vue },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...vue.configs['flat/recommended'].rules,
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+  {
+    files: ['tests/**/*.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.vitest,
+      },
+    },
+  },
+]
