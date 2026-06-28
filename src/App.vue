fdc2e46e3f7f28f92a3eb1c96e34076bbd1de3fc
aminsadiq350 <aminsadiq@MacBook-Pro-16>
2026-06-28 14:15:25 +0400

    feat(m1): mount app shell

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AppSidebar from './components/AppSidebar.vue'
+import PdfViewer from './components/PdfViewer.vue'
+import ChatPanel from './components/ChatPanel.vue'
+import Toast from './components/ui/Toast.vue'
+</script>
+
+<template>
+  <div class="h-full flex overflow-hidden relative">
+    <AppSidebar />
+    <PdfViewer />
+    <ChatPanel />
+    <Toast />
+  </div>
+</template>
